f2fc475b2a34ac5c62c1944286617bda66d01a35
Md Modassir <modassir.studio@gmail.com>
2026-09-22 09:10:07 +0530

    Add files via upload

---
+++ CodeWithHarry_Exercises/Lectures/CWH_04_Literals.java
@@ -0,0 +1,17 @@
+package Code_With_Modassir;
+
+public class CWH_04_Literals {
+    public static void main(String[] args) {
+        byte age = 101;
+        int age2 = 31;
+        short age3 = 11;
+        long ageDino = 566666666666l;
+        char gender = 'A';
+        double dec1 = 4.4;
+        float dec2 = 3.44f;
+        boolean a = true;
+        System.out.println(age);
+        String str = "CodeWithMod";
+        System.out.println(str);
+    }
+}
